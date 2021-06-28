91a577d6e9e19501e40b7795abf3ec230ca4a998
Chehui Chou <deadshot465@gmail.com>
2021-06-28 18:51:18 +0900

    Initial commit.

---
+++ src/Ch5.purs
@@ -0,0 +1,116 @@
+module Ch5 where
+
+import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude (Unit, discard, show, (+), (-), (<), negate, (>=), (/=))
+
+test :: Effect Unit
+test = do
+  log $ show $ flip const 1 2
+  flip const 1 2 # show # log
+  log $ show $ null Nil
+  log $ show $ null ("abc" : Nil)
+  log $ show $ snoc (1 : 2 : Nil) 3
+  log $ show $ length $ 1 : 2 : 3 : Nil
+  log $ show $ length' $ 1 : 2 : 3 : Nil
+  log $ show (head Nil :: Maybe Unit)
+  log $ show $ head ("abc" : "123" : Nil)
+  log $ show $ tail (Nil :: List Unit)
+  log $ show $ tail ("abc" : "123" : Nil)
+  log $ show $ last (Nil :: List Unit)
+  log $ show $ last ("a" : "b" : "c" : Nil)
+  log $ show $ init (Nil :: List Unit)
+  log $ show $ init (1 : Nil)
+  log $ show $ init (1 : 2 : Nil)
+  log $ show $ init (1 : 2 : 3 : Nil)
+  log $ show $ uncons (1 : 2 : 3 : Nil)
+  log $ show $ index (1 : Nil) 4
+  log $ show $ index (1 : 2 : 3 : Nil) 1
+  log $ show $ index (Nil :: List Unit) 0
+  log $ show $ index (1 : 2 : 3 : Nil) (-99)
+  log $ show $ (1 : 2 : 3 : Nil) !! 1
+  log $ show $ findIndex (_ >= 2) (1 : 2 : 3 : Nil)
+  log $ show $ findIndex (_ >= 99) (1 : 2 : 3 : Nil)
+  log $ show $ findIndex (10 /= _) (Nil :: List Int)
+
+flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
+flip f x y = f y x
+
+const :: ∀ a b. a -> b -> a
+const x _ = x
+
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+
+infixr 0 apply as $
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+applyFlipped x f = f x
+
+infixl 0 applyFlipped as #
+
+singleton :: ∀ a. a -> List a
+singleton x = x : Nil
+
+null :: ∀ a. List a -> Boolean
+null Nil = true
+null _ = false
+
+snoc :: ∀ a. List a -> a -> List a
+snoc Nil x = singleton x
+snoc (y : ys) x = y : snoc ys x
+
+length :: ∀ a. List a -> Int
+length Nil = 0
+length (_ : xs) = 1 + length xs
+
+length' :: ∀ a. List a -> Int
+length' l = go l 0
+  where
+    go :: ∀ b. List b -> Int -> Int
+    go Nil n = n
+    go (_ : xs') n = go xs' (n + 1)
+
+head :: ∀ a. List a -> Maybe a
+head (x : _) = Just x
+head _ = Nothing
+
+tail :: ∀ a. List a -> Maybe (List a)
+tail (_ : xs) = Just xs
+tail _ = Nothing
+
+last :: ∀ a. List a -> Maybe a
+last Nil = Nothing
+last (x : Nil) = Just x
+last (_ : xs) = last xs
+
+init :: ∀ a. List a -> Maybe (List a)
+init Nil = Nothing
+init l = Just $ go l
+  where
+    go :: ∀ b. List b -> List b
+    go Nil = Nil
+    go (_ : Nil) = Nil
+    go (x : xs) = x : go xs
+
+uncons :: ∀ a. List a -> Maybe { head :: a, tail :: List a }
+uncons Nil = Nothing
+uncons (x : xs) = Just { head: x, tail: xs }
+
+index :: ∀ a. List a -> Int -> Maybe a
+index Nil _ = Nothing
+index _ i | i < 0 = Nothing
+index (x : _) 0 = Just x
+index (_ : xs) i = index xs (i - 1)
+
+infixl 8 index as !!
+
+findIndex :: ∀ a. (a -> Boolean) -> List a -> Maybe Int
+findIndex _ Nil = Nothing
+findIndex pred l = go 0 l
+  where
+    go acc (x : _) | pred x = Just acc
+    go _ Nil = Nothing
+    go acc (_ : xs) = go (acc + 1) xs
---
+++ src/Main.purs
@@ -0,0 +1,9 @@
+module Main where
+
+import Prelude
+
+import Ch5 as Ch5
+import Effect (Effect)
+
+main :: Effect Unit
+main = Ch5.test
