1bb5e015234674b81e87baa3beb773b51c2675ff
Chehui Chou <deadshot465@gmail.com>
2021-07-01 17:49:20 +0900

    Finish basic parts.

--- spago.dhall
+++ spago.dhall
@@ -12,7 +12,7 @@ to generate this file without the comments in this block.
 -}
 { name = "my-project"
 , dependencies =
-  [ "console", "effect", "lists", "maybe", "prelude", "psci-support" ]
+  [ "console", "effect", "lists", "maybe", "prelude", "psci-support", "tuples" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Ch5.purs
+++ src/Ch5.purs
@@ -2,32 +2,40 @@ module Ch5 where
 
 import Data.List (List(..), (:))
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..), snd)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (
-  Unit, discard, negate, otherwise, show, (+), (-), (/=), (<), (>=), (==), 
-  type (~>), (>), (<<<))
+import Prelude (type (~>), Unit, discard, max, negate, otherwise, show, (+), (-), (/=), (<), (<<<), (>>>), (==), (>), (>=))
 
 test :: Effect Unit
 test = do
   log $ show $ flip const 1 2
   flip const 1 2 # show # log
+  log "Null --"
   log $ show $ null Nil
   log $ show $ null ("abc" : Nil)
+  log "Snoc --"
   log $ show $ snoc (1 : 2 : Nil) 3
+  log "Length --"
   log $ show $ length $ 1 : 2 : 3 : Nil
   log $ show $ length' $ 1 : 2 : 3 : Nil
+  log "Head --"
   log $ show (head Nil :: Maybe Unit)
   log $ show $ head ("abc" : "123" : Nil)
+  log "Tail --"
   log $ show $ tail (Nil :: List Unit)
   log $ show $ tail ("abc" : "123" : Nil)
+  log "Last --"
   log $ show $ last (Nil :: List Unit)
   log $ show $ last ("a" : "b" : "c" : Nil)
+  log "Init --"
   log $ show $ init (Nil :: List Unit)
   log $ show $ init (1 : Nil)
   log $ show $ init (1 : 2 : Nil)
   log $ show $ init (1 : 2 : 3 : Nil)
+  log "Uncons --"
   log $ show $ uncons (1 : 2 : 3 : Nil)
+  log "Index --"
   log $ show $ index (1 : Nil) 4
   log $ show $ index (1 : 2 : 3 : Nil) 1
   log $ show $ index (Nil :: List Unit) 0
@@ -48,6 +56,39 @@ test = do
   log "Filter --"
   log $ show $ filter (4 > _) $ (1 : 2 : 3 : 4 : 5 : 6 : Nil)
   log $ show $ filter' (4 > _) $ (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+  log "CatMaybes --"
+  log $ show $ catMaybes (Just 1 : Nothing : Just 2 : Nothing : Nothing : Just 5 : Nil)
+  log "Range --"
+  log $ show $ range 1 10
+  log $ show $ range 3 (-3)
+  log $ show $ range' 1 10
+  log $ show $ range' 3 (-3)
+  log "Take --"
+  log $ show $ take 5 (12 : 13 : 14 : Nil)
+  log $ show $ take 5 (-7 : 9 : 0 : 12 : -13 : 45 : 976 : -19 : Nil)
+  log "Drop --"
+  log $ show $ drop 2 (1 : 2 : 3 : 4 : 5 : 6 : 7 : Nil)
+  log $ show $ drop 10 (Nil :: List Unit)
+  log "TakeWhile --"
+  log $ show $ takeWhile (_ > 3) (5 : 4 : 3 : 99 : 101 : Nil)
+  log $ show $ takeWhile (_ == -17) (1 : 2 : 3 : Nil)
+  log "DropWhile --"
+  log $ show $ dropWhile (_ > 3) (5 : 4 : 3 : 99 : 101 : Nil)
+  log $ show $ dropWhile (_ == -17) (1 : 2 : 3 : Nil)
+  log "TakeEnd --"
+  log $ show $ takeEnd 3 (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+  log $ show $ takeEnd 10 (1 : Nil)
+  log "DropEnd --"
+  log $ show $ dropEnd 3 (1 : 2 : 3 : 4 : 5 : 6 : Nil)
+  log $ show $ dropEnd 10 (1 : Nil)
+  log "Zip --"
+  log $ show $ zip (1 : 2 : 3 : Nil) ("a" : "b" : "c" : "d" : "e" : Nil)
+  log $ show $ zip ("a" : "b" : "c" : "d" : "e" : Nil) (1 : 2 : 3 : Nil)
+  log $ show $ zip (Nil :: List Unit) (1 : 2 : Nil)
+  log "Unzip --"
+  log $ show $ unzip (Tuple 1 "a" : Tuple 2 "b" : Tuple 3 "c" : Nil)
+  log $ show $ unzip (Tuple "a" 1 : Tuple "b" 2 : Tuple "c" 3 : Nil)
+  log $ show $ unzip (Nil :: List (Tuple Unit Unit))
 
 flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
@@ -161,3 +202,66 @@ filter' pred = reverse <<< go Nil
   where
     go acc Nil = acc
     go acc (x : xs) = if pred x then go (x : acc) xs else go acc xs
+
+catMaybes :: ∀ a. List (Maybe a) -> List a
+catMaybes Nil = Nil
+catMaybes ((Just x) : xs) = x : catMaybes xs
+catMaybes (Nothing : xs) = catMaybes xs
+
+range :: Int -> Int -> List Int
+range start end | end > start = start : range (start + 1) end
+                | end < start = start : range (start - 1) end
+                | otherwise = singleton end
+
+range' :: Int -> Int -> List Int
+range' start end = go Nil end start
+  where
+    go acc start' end' | start' == end' = end' : acc
+                       | otherwise = go (start' : acc) (start' + step) end'
+    step = if end > start then (-1) else 1
+
+take :: ∀ a. Int -> List a -> List a
+take n = reverse <<< go Nil (max 0 n)
+  where
+    go :: List a -> Int -> List a -> List a
+    go acc 0 _ = acc
+    go acc _ Nil = acc
+    go acc n' (x : xs) = go (x : acc) (n' - 1) xs
+
+drop :: ∀ a. Int -> List a -> List a
+drop 0 l = l
+drop _ Nil = Nil
+drop n (_ : xs) = drop (n - 1) xs
+
+takeWhile :: ∀ a. (a -> Boolean) -> List a -> List a
+takeWhile pred = reverse <<< go Nil pred
+  where
+    go acc _ Nil = acc
+    go acc pred' (x : xs) = if pred' x then go (x : acc) pred' xs else acc
+
+dropWhile :: ∀ a. (a -> Boolean) -> List a -> List a
+dropWhile _ Nil = Nil
+dropWhile pred l@(x : xs) = if pred x then dropWhile pred xs else l
+
+takeEnd :: ∀ a. Int -> List a -> List a
+takeEnd n = go >>> snd
+  where
+    go Nil = Tuple 0 Nil
+    go (x : xs) = go xs # \(Tuple c nl) -> Tuple (c + 1) $ if c < n then x : nl else nl
+
+dropEnd :: ∀ a. Int -> List a -> List a
+dropEnd n = go >>> snd
+  where
+    go Nil = Tuple 0 Nil
+    go (x : xs) = go xs # \(Tuple c nl) -> Tuple (c + 1) $ if c < n then nl else x : nl
+
+zip :: ∀ a b. List a -> List b -> List (Tuple a b)
+zip l = reverse <<< go Nil l
+  where
+    go acc Nil _ = acc
+    go acc _ Nil = acc
+    go acc (x : xs) (y : ys) = go (Tuple x y : acc) xs ys
+
+unzip :: ∀ a b. List (Tuple a b) -> Tuple (List a) (List b)
+unzip Nil = Tuple Nil Nil
+unzip (Tuple x y : ts) = unzip ts # \(Tuple xs ys) -> Tuple (x : xs) (y : ys)
