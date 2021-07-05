542ecd4969e31fb3807b4370d238c15c9fda4898
Chehui Chou <deadshot465@gmail.com>
2021-07-05 10:41:21 +0900

    Complete Ch9.

--- src/Ch9.purs
+++ src/Ch9.purs
@@ -2,6 +2,7 @@ module Ch9 where
 
 import Data.Eq (class Eq)
 import Data.Generic.Rep (class Generic)
+import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
@@ -18,6 +19,10 @@ test = do
   verifyAndBoolMonoid
   verifyOrBoolSemigroup
   verifyOrBoolMonoid
+  verifyMod4Semigroup
+  verifyMod4Monoid
+  log $ show $ First Nothing <> First (Just 77)
+  log $ show $ Last (Just 1) <> Last (Just 99)
 
 class Semigroup a where
   append :: a -> a -> a
@@ -77,3 +82,79 @@ verifyOrBoolMonoid = do
   log "Verifying OrBool Monoid Laws (2 tests)"
   log $ show $ OTrue <> mempty == mempty <> OTrue && OTrue <> mempty == OTrue
   log $ show $ OFalse <> mempty == mempty <> OFalse && OFalse <> mempty == OFalse
+
+data Mod4 = Zero | One | Two | Three
+
+infixr 5 append as +
+
+instance semigroupMod4 :: Semigroup Mod4 where
+  append Zero x = x
+  append x Zero = x
+  append One One = Two
+  append One Two = Three
+  append One Three = Zero
+  append Two One = Three
+  append Two Two = Zero
+  append Two Three = One
+  append Three One = Zero
+  append Three Two = One
+  append Three Three = Two
+
+instance monoidMod4 :: Monoid Mod4 where
+  mempty = Zero
+
+class Group g where
+  ginverse :: g -> g
+
+instance groupMod4 :: Group Mod4 where
+  ginverse One = Three
+  ginverse Two = Two
+  ginverse Three = One
+  ginverse Zero = Zero
+
+derive instance eqMod4 :: Eq Mod4
+derive instance genericMod4 :: Generic Mod4 _
+instance showMod4 :: Show Mod4 where
+  show = genericShow
+
+verifyMod4Semigroup :: Effect Unit
+verifyMod4Semigroup = do
+  log "Verifying Mod4 Semigroup Laws (1 test)"
+  log $ show $ (One + Two) + Three == One + (Two + Three)
+
+verifyMod4Monoid :: Effect Unit
+verifyMod4Monoid = do
+  log "Verifying Mod4 Monoid Laws (4 tests)"
+  log $ show $ One + mempty == mempty + One && One + mempty == One
+  log $ show $ Two + mempty == mempty + Two && Two + mempty == Two
+  log $ show $ Three + mempty == mempty + Three && Three + mempty == Three
+  log $ show $ Zero + mempty == mempty + Zero && Zero + mempty == Zero
+
+newtype First a = First (Maybe a)
+newtype Last a = Last (Maybe a)
+
+derive newtype instance eqFirst :: Eq a => Eq (First a)
+derive newtype instance eqLast :: Eq a => Eq (Last a)
+
+derive instance genericFirst :: Generic (First a) _
+derive instance genericLast :: Generic (Last a) _
+
+instance showFirst :: Show a => Show (First a) where
+  show = genericShow
+
+instance showLast :: Show a => Show (Last a) where
+  show = genericShow
+
+instance semigroupFirst :: Semigroup (First a) where
+  append (First Nothing) last = last
+  append first _ = first
+
+instance monoidFirst :: Monoid (First a) where
+  mempty = First Nothing
+
+instance semigroupLast :: Semigroup (Last a) where
+  append first (Last Nothing) = first
+  append _ last = last
+
+instance monoidLast :: Monoid (Last a) where
+  mempty = Last Nothing
