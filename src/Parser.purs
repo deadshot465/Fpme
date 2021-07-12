81f795cf200d96456e282cd28c7198d1987d6734
Chehui Chou <deadshot465@gmail.com>
2021-07-12 18:15:49 +0900

    Add monadic parser.

--- src/Parser.purs
+++ src/Parser.purs
@@ -41,12 +41,22 @@ newtype Parser e a = Parser (ParserFunction e a)
 instance functorParser :: Functor (Parser e) where
   map f p = Parser \s -> (map f) <$> parse p s
 
+instance bindParser :: Bind (Parser e) where
+  {- bind p f = Parser \s -> do
+    Tuple s1 x <- parse p s
+    parse (f x) s1 -}
+  --bind p f = Parser \s -> parse p s >>= \(Tuple s1 x) -> parse (f x) s1
+  bind p f = Parser (parse p >=> \(Tuple s1 x) -> parse (f x) s1)
+
+instance monadParser :: Monad (Parser e)
+
 instance applyParser :: Apply (Parser e) where
-  apply p1 p2 = Parser \s -> case parse p1 s of
+  {- apply p1 p2 = Parser \s -> case parse p1 s of
     Left err -> Left err
     Right (Tuple s1 h) -> case parse p2 s1 of
       Left err -> Left err
-      Right (Tuple s2 x) -> Right $ Tuple s2 (h x)
+      Right (Tuple s2 x) -> Right $ Tuple s2 (h x) -}
+  apply = ap
 
 instance applicativeParser :: Applicative (Parser e) where
   pure x = Parser \s -> pure $ Tuple s x
